3d6c3ea17d2e07075458e195a19e4bd6446d3d03
Efren Acosta <aquiefrenacosta@gmail.com>
2019-02-28 09:26:01 -0500

    first commit

---
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Place } from '../../place.model';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
+
+@Component({
+  selector: 'app-new-offer',
+  templateUrl: './new-offer.page.html',
+  styleUrls: ['./new-offer.page.scss'],
+})
+export class NewOfferPage implements OnInit {
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+}
---
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../../places.service';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { Place } from '../../place.model';
+
+@Component({
+  selector: 'app-offer-bookings',
+  templateUrl: './offer-bookings.page.html',
+  styleUrls: ['./offer-bookings.page.scss'],
+})
+export class OfferBookingsPage implements OnInit {
+  place: Place;
+
+  constructor(
+    private route: ActivatedRoute,
+    private navCtr: NavController,
+    private placesService: PlacesService    
+  ) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtr.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });    
+  }
+
+}
